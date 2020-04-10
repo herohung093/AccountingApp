595b093c68e4d15b902f7ec92027eb07e3f45bf7
Hector <hung.vm2574@gmail.com>
2020-04-11 09:57:38 +1000

    change base url backend

--- src/common/baseUrl.tsx
+++ src/common/baseUrl.tsx
@@ -1,4 +1,6 @@
 const baseUrl = {
-    base: "https://stormy-ridge-84291.herokuapp.com/"
+    //base: "https://stormy-ridge-84291.herokuapp.com/"
+    base: "http://chudragon-env.eba-xti4hstm.us-east-1.elasticbeanstalk.com/"
+    //base: "http://localhost:8080/"
 }
 export default baseUrl
